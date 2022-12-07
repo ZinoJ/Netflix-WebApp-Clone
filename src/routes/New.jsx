b14abc2fc5d47fab23c7c1337b43aa7eadf120e7
ZinoJ <joshuaubiri@gmail.com>
2022-12-08 00:30:16 +0100

    news

---
+++ src/routes/New.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function New() {
+  return (
+    <div>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam, natus suscipit adipisci et odio eaque deserunt eveniet saepe incidunt quasi aliquid. Odit vitae dolor quidem consectetur ipsum.</div>
+  )
+}
+
+export default New
